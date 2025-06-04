67a72c6158eb9d5171266f4e562836415e5783a5
lucasziiN <lucassoda2@gmail.com>
2025-06-04 23:47:22 +1200

    Added time measurement

--- A4.java
+++ A4.java
@@ -17,10 +17,18 @@ public class A4 {
             // Allocate an int array matching the current size in bytes
             int[] arr = new int [i/4]; // Divide by 4 since each int is 4 bytes
 
-            // Write to arrays
+            long start = System.nanoTime();
+
+            // Access one value per 64-byte cache line to load data into cache
             for (int j = 0; j<arr.length; j += step){
                 arr[j]++;
             }
+
+            long end = System.nanoTime();
+
+            // Calculate and print total access time (in ms) for the current array size
+            double timeMs = (end-start) / 1_000_000.0;
+            System.out.printf("%8d KB\t%.4f ms\n", i / 1024, timeMs);
         }
     }
 }
