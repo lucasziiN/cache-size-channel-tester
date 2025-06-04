7488d74f9b7434edf25abbdc7c53c1093d95d6bc
lucasziiN <lucassoda2@gmail.com>
2025-06-04 23:36:37 +1200

    Implement memory allocation from 4KB to 256MB

--- A4.java
+++ A4.java
@@ -3,7 +3,18 @@ import java.util.Arrays;
 public class A4 {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         
+        // Set the limit size for the arrays (256MB)
+        int MAX_SIZE = 256 * 1024 * 1024;
+
+        // Define the memory stride in bytes 
+        int STRIDE = 64; 
+        
+        // Loop through increasing array sizes from 4 KB to MAX_SIZE
+        for (int i = 4096; i <= MAX_SIZE; i *=2){
+            // Allocate an int array matching the current size in bytes
+            int[] arr = new int [i/4]; // Divide by 4 since each int is 4 bytes
+
+        }
     }
 }
