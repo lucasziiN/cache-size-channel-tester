05b5bd17582aa954e809996f2030ca356e904e84
lucasziiN <lucassoda2@gmail.com>
2025-06-05 19:17:55 +1200

    Ensured each array regardless of its size is accessed the same amount of times

--- A4.java
+++ A4.java
@@ -6,12 +6,14 @@ public class A4 {
         
         // Set the limit size for the arrays (256MB)
         int MAX_SIZE = 512 * 1024 * 1024;
+        int totalAccesses = MAX_SIZE/4;
 
         // Define the memory stride in bytes 
         int STRIDE = 64; 
         
-        int step = STRIDE / 4;
+        int step = STRIDE / 4; // since each int is 4 bytes, step = 16 ints
 
+        // Number of times to repeat the measurement for averaging
         int LOOPS = 10;
 
         // Loop through increasing array sizes from 4 KB to MAX_SIZE
@@ -20,18 +22,25 @@ public class A4 {
             int[] arr = new int [i/4]; // Divide by 4 since each int is 4 bytes
 
             long totalTime = 0;
+            int accessSum = 0; // used to accumulate reads/writes so JIT can't skip the loop
+
 
             // Run the same test multiple times to get an average result since it will mitigate the effect of outliers
             for (int k = 0; k < LOOPS; k++){
                 long start = System.nanoTime();
                 
-                // Access one value per 64-byte cache line to load data into cache
-                for (int j = 0; j<arr.length; j += step){
-                    arr[j]++;
+                // Repeat the “touch-every-cache-line” loop enough times so that each array (regardless of its size) sees roughly 'totalAccesses' int-accesses
+                for (int accessed = 0; accessed < totalAccesses; accessed += arr.length) {
+                    // Full pass over this array
+                    for (int j = 0; j < arr.length; j += step) {
+                        arr[j]++;
+                        accessSum += arr[j]; // Add each value to accessSum so the loop can’t be removed by the compiler
+                    }
                 }
+            
 
                 long end = System.nanoTime();
-                totalTime += (end-start);
+                totalTime += (end-start); // Compute time taken 
             }
             
 
